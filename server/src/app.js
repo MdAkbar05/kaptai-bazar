7f22316dd9fcd3319545862b3ce6454f637883d4
MdAkbar05 <samratakbar667466@gmail.com>
2025-02-08 00:39:39 +0600

    updated

--- server/src/app.js
+++ server/src/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "http://localhost:3001", // Your frontend URL
+    origin: process.env.CLIENT_URL, // Your frontend URL
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   })
 );
